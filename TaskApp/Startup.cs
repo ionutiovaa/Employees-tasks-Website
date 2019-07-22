cfd5b0d7dd2bf863003e9ae020816349da717cba
ionutiovaa <yionut_iova@yahoo.com>
2019-07-22 10:16:25 +0300

    Create TaskApp

---
+++ TaskApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskApp.Startup))]
+namespace TaskApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
